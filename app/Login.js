8cefe0bd17452f6097a5c29cc4cb544f6d025f91
sedama29 <sathwikareddy0799@gmail.com>
2024-01-25 12:20:55 -0600

    working application

--- app/Login.js
+++ app/Login.js
@@ -1,19 +1,19 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-
 GoogleSignin.configure({
-  androidClientId: '591454203526-fcil5gkg6hn6h6nnrrbkojmopk2jof00.apps.googleusercontent.com',
-  webClientId: '591454203526-gnjttnc4drp063ttv459ldplrca4hfag.apps.googleusercontent.com',
-});
+    androidClientId: '591454203526-fcil5gkg6hn6h6nnrrbkojmopk2jof00.apps.googleusercontent.com',
+    webClientId: '591454203526-gnjttnc4drp063ttv459ldplrca4hfag.apps.googleusercontent.com',
+  });
 
 export const signInWithGoogle = async () => {
   try {
-
-    // await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin[Platform.OS === 'android' ? 'signIn' : 'signInSilently']();
     console.log('User Info:', userInfo);
     await GoogleSignin.revokeAccess();
+    return userInfo; 
   } catch (error) {
     console.error('Google Sign-In Error:', error);
+    throw error; 
   }
 };
+
